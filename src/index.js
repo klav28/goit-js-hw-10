d11db4d556c74be0e776fd6b422bf4297e375319
klav28 <klav28@gmail.com>
2023-03-27 20:30:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@ import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
 
+const elInputField = document.querySelector("#search-box");
+
+function handleInput(ev) {
+    console.log(ev.target.value);
+}
+
+elInputField.addEventListener("input", handleInput);
