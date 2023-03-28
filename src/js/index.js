b644789dd97a5fc8d18eb436ebe13cb3f5dbd68a
klav28 <klav28@gmail.com>
2023-03-28 12:02:50 +0300

    markup update

--- src/js/index.js
+++ src/js/index.js
@@ -34,49 +34,51 @@ function onFetchError() {
     Notiflix.Notify.failure("Oops, there is no country with that name");
 };
 
-
-function renderMarkup(country) {
-
-    if (country.length > MAX_COUNTRIES_TO_SHOW) {
-        Notiflix.Notify.info(`Too many (${country.length}) matches found. Please enter a more specific name.`);
-        return;
-    }
-
-    if (country.length > 1) {
-        const countryList = country.reduce((acc,el) => {
+function renderCountriesList(countries) {
+        return countries.reduce((acc,el) => {
             return acc +
                 `<li class="country-list__item">
                     <img class="flag__item" src=${el.flags.svg} alt="${el.name.common}" width="96"</ >
                     <p class="country-list__txt">${el.name.official}</p>
                 </li>`
         }, "");
-        elCountryList.innerHTML = countryList;
-        return;
-    }
+}
 
-    const countryDetail = `
+function renderCountryDetail (country) {
+    return `
         <div class="country-info__container">
-            <img class="flag__item" src=${country[0].flags.svg} alt="${country[0].name.common}" width="96"</ >
-            <p class="country-info__header">${country[0].name.official}</p>
+            <img class="flag__item" src=${country.flags.svg} alt="${country.name.common}" width="96"</ >
+            <p class="country-info__header">${country.name.official}</p>
         </div>
         <ul class="country-info__list">
             <li>
                 <p class="country-info__subheader">Capital: 
-                <span class="country-info__detail">${country[0].capital}</span></p>
+                <span class="country-info__detail">${country.capital}</span></p>
             </li >
             <li>
                 <p class="country-info__subheader">Population:
-                <span class="country-info__detail">${country[0].population}</span></p>                
+                <span class="country-info__detail">${country.population}</span></p>                
             </li>
             <li>
                 <p class="country-info__subheader">Languages:
-                <span class="country-info__detail">${Object.values(country[0].languages).join(", ")}</span></p>
+                <span class="country-info__detail">${Object.values(country.languages).join(", ")}</span></p>
             </li>
         </ul>
     `;
+}
 
-    elCountryInfo.innerHTML = countryDetail;
-    return;
+function renderMarkup(countries) {
+    if (countries.length > MAX_COUNTRIES_TO_SHOW) {
+            Notiflix.Notify.info(`Too many (${countries.length}) matches found. Please enter a more specific name.`);
+        return;
+    }
+
+    if (countries.length > 1) {
+        elCountryList.innerHTML = renderCountriesList(countries);
+    } else {
+        elCountryInfo.innerHTML = renderCountryDetail(countries[0]);
+    }
+
 }
 
 elInputField.addEventListener("input", debounce(handleInput, DEBOUNCE_DELAY));
